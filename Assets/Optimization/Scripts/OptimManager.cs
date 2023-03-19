b696254d9db3568ad5e67ca5297106b2d5c513c0
Adam Chyb <achybby@gmail.com>
2023-03-19 16:19:55 +1100

    Optimize.

--- Assets/Optimization/Scripts/OptimManager.cs
+++ Assets/Optimization/Scripts/OptimManager.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OptimManager : MonoBehaviour
-{
+public class OptimManager : MonoBehaviour {
     public OptimUnit OptimPrefab;
     public int InstanceCount;
     public Vector3 Size;
-    
+
     // Start is called before the first frame update
-    void Start()
-    {
-        for (int i = 0; i < InstanceCount; ++i)
-        {
+    void Start() {
+        for (int i = 0; i < InstanceCount; ++i) {
             Vector3 position = new Vector3(
             Random.Range(-Size.x, Size.x),
             Random.Range(-Size.y, Size.y),
